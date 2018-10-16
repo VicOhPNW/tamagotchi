b6997e3606c7c81d203a265592d85c8568d1d245
David Mortkowitz and Victoria Oh <victoria@>
2018-10-16 14:16:36 -0700

    Add dead/alive images. Add Giphy API.

--- src/main.js
+++ src/main.js
@@ -7,16 +7,28 @@ import img1 from "./tamaalive.jpg";
 import img2 from "./tamadead.jpg";
 
 $(document).ready(function() {
-  console.log(img1);
   $("#alive").attr("src", img1);
   $("#dead").attr("src", img2);
+
+  $.ajax({
+   url: `https://api.giphy.com/v1/gifs/random?api_key=BU3f5x3Dt9ntdRhwUGdidx1jLgcgsZTu&tag=&rating=G`,
+   type: 'GET',
+   data: {
+     format: 'json'
+   },
+   success: function(response) {
+     $('#gif').append(`<img src=${response.data.images.fixed_height_downsampled.url} alt="some random gif">`);
+   },
+   error: function() {
+     $('#error').text("There was an error processing your request. Please try again.");
+   }
+ });
 
   $("#tamagotchiName").submit(function(event){
     event.preventDefault();
 
     let name = $("#tamagotchiInput").val();
     let newTamagotchi = new Tamagotchi(name);
-
 
     newTamagotchi.setHunger();
     newTamagotchi.setSleep();
